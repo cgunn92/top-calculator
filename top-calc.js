26fc4391204ca2cc289543fe146d73698934e680
cgunn92 <nominetrinitatis@gmail.com>
2022-11-11 15:11:32 -0800

    Make event listeners for buttons to push to array

--- top-calc.js
+++ top-calc.js
@@ -1,5 +1,65 @@
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.displayContainer');
 
+let operators = [];
 
+buttons.forEach(button => button.addEventListener('click', function(e){
+    
+    switch(button.className){
+        case 'clear':
+            while(operators.length != 0){
+                operators.pop()
+            }
+            break;
+        case 'divide':
+            operators.push('//');
+            break;
+        case 'seven':
+            operators.push(7);
+            break;
+        case 'eight':
+            operators.push(8);
+            break;
+        case 'nine':
+            operators.push(9);
+            break;
+        case 'four':
+            operators.push(4);
+            break;
+        case 'five':
+            operators.push(5);
+            break;
+        case 'six':
+            operators.push(6);
+            break;
+        case 'one':
+            operators.push(1);
+            break;
+        case 'two':
+            operators.push(2);
+            break;
+        case 'three':
+            operators.push(3);
+            break;
+        case 'multiply':
+            operators.push('*');
+            break;
+        case 'add':
+            operators.push('+');
+            break;
+        case 'subtract':
+            operators.push('-');
+            break;
+        case 'zero':
+            operators.push(0);
+            break;
+        case 'dot':
+            operators.push('.');
+            break;
+    }
+
+    console.log(operators);
+}));
 
 const add = function(num1, num2) {
 	return num1 + num2;
